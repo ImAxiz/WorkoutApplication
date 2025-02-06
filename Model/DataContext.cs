e261790d31e3e75f29f7faa129822c60db7b8593
savva <158562304+ImAxiz@users.noreply.github.com>
2025-02-06 14:09:52 +0200

    Add project files.

---
+++ Model/DataContext.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace WorkoutApplication.Model
+{
+    public class DataContext : DbContext
+    {
+        public  DataContext(DbContextOptions<DataContext> options) : base(options) 
+        { 
+        }
+        public DbSet<Exercise>? ExerciseList { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<Exercise>().HasData(
+                new Exercise
+                {
+                    Id = 1,
+                    Title = "Kätekõverdused jala tõstega",
+                    Description = "Tabalised kätekõverdused korda mööda jalga tõstes",
+                    Intensity = Exercise.ExerciseIntensity.Normal,
+                    RecommendedDurationInSeconds = 40,
+                    RecommendedTimeInSecondsBeforeExercise = 10,
+                    RecommendedTimeInSecondsAfterExercise = 10,
+                },
+                new Exercise
+                {
+
+                });
+        }
+
+    }
+}
